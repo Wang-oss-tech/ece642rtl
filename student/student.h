ef1c38d8b3469f6ec3d1d73b4f566cc34cf86f24
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-09-18 21:56:00 -0400

    reverting

--- student/student.h
+++ student/student.h
@@ -22,6 +22,6 @@ int translateOrnt(int orientation, turtleMove nextMove);
 turtleMove studentTurtleStep(bool bumped);
 
 // OK to change below this line
-bool studentMoveTurtle(QPointF& position, int32_t& orientation);
+bool studentMoveTurtle(QPointF& position, int& orientation);
 
 
