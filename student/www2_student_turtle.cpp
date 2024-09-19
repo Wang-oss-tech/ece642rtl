caa2ebf55de0e3ea02b2972d9405af9f104a3f89
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-09-18 21:26:31 -0400

    enum error

--- student/www2_student_turtle.cpp
+++ student/www2_student_turtle.cpp
@@ -37,7 +37,7 @@ typedef int32_t State;       // Typedef for state representation
 typedef bool Flag;           // Typedef for boolean flags
 
 // Static Array to keep track visits to each cell
-static int visitMap[MAZE_SIZE][MAZE_SIZE] = {0} // all cells initialized to zero
+static int32_t visitMap[MAZE_SIZE][MAZE_SIZE] = {0}; // all cells initialized to zero
 
 // Enum to represent directions
 enum Direction {
@@ -83,15 +83,16 @@ typedef struct {
 /**
  * @brief Function to get number of visits to specific cell
  */
-int32_t getVisits(int32_t x, int32_t y){
+int32_t getVisits(int32_t x, int32_t y) {
     return visitMap[x][y];
 }
 
 /**
  * @brief Function to increment the number of visits to a specific cell
  */
-void incrementVisits(int32_t x, int32_t y):
+void incrementVisits(int32_t x, int32_t y) {
     visitMap[x][y]++;
+}
 
 /**
  * @brief Checks the turtle's direction and updates its orientation and state.
