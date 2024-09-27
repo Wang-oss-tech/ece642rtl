987eab143f01af9d859eaf2fddf4d48a52ce1574
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-09-27 13:34:22 -0400

    TRYING AGAIn

--- student/www2_student_maze.cpp
+++ student/www2_student_maze.cpp
@@ -111,15 +111,16 @@ bool moveTurtle(QPointF& pos_, int& nw_or)
     // Call to studentTurtleStep() to determine next step based on whether a bump occurred
     turtleMove nextMove = studentTurtleStep(bumpedFlag);
 
+    if ((nextMove == MOVE_FORWARD) && !atEnd) {
+      for (int32_t i = 0; i < TIMEOUT; i ++);
+      pos_ = translatePos(pos_, nextMove, old_nw_or);            // updates Position
+      ROS_INFO("UPDATE POSITION at this tick (X, Y): %f, %f", pos_.x(), pos_.y());
+    }
     ROS_INFO("nextMove: %d", nextMove);
     ROS_INFO("Orientation: %d", nw_or);
     nw_or = translateOrnt(nw_or, nextMove); // update orientation
     ROS_INFO("UPDATE ORIENTATION AT THIS TICK: %d", nw_or);
-
-    if ((nextMove == MOVE_FORWARD) && !atEnd) {
-      pos_ = translatePos(pos_, nextMove, old_nw_or);            // updates Position
-      ROS_INFO("UPDATE POSITION at this tick (X, Y): %f, %f", pos_.x(), pos_.y());
-    }
+    
     ROS_INFO("Position at this tick (X, Y): %f, %f", pos_.x(), pos_.y());
   }
 
