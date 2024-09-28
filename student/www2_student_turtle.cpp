69a3a02be512972eaa7c37c26524ee4d0410ee35
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-09-28 02:35:02 -0400

    visit attempts

--- student/www2_student_turtle.cpp
+++ student/www2_student_turtle.cpp
@@ -94,6 +94,12 @@ turtleMove studentTurtleStep(bool bumped) {
     static State currentState = STATE_MOVE_FORWARD; // Current state of the turtle's movement
     
     ROS_INFO("Student turtle step called Orig State: %d", currentState);
+    if (currentX != prevX || currentY != prevY) {
+        incrementVisits(currentX, currentY);  // Increment the visit count for the new cell
+        prevX = currentX;   // Update previous position
+        prevY = currentY;
+    }
+    
     // returns the move back to maze on what to do (depends on current state & whether it has bumped)
     if (currentState == STATE_MOVE_FORWARD){
         currentState = STATE_TURN_RIGHT;
