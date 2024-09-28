55e1d425348ba63c06530b09caab9ad3d52e98b8
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-09-27 23:47:38 -0400

    two commits ago was kind of working

--- student/www2_student_maze.cpp
+++ student/www2_student_maze.cpp
@@ -119,7 +119,7 @@ bool moveTurtle(QPointF& pos_, int& nw_or)
     shouldMove = (nextMove == MOVE_FORWARD);
 
     if (shouldMove && !atEnd) {
-      pos_ = translatePos(pos_, nextMove, nw_or);            // updates Position
+      pos_ = translatePos(pos_, nextMove, old_nw_or);            // updates Position
       ROS_INFO("UPDATE POSITION (X, Y): %f, %f", pos_.x(), pos_.y());
       shouldMove = false;
     }
