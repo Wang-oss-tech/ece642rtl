9320e3c98fbf450f1c33fa75093f7ff7bdb3530f
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-10-24 15:18:48 -0400

    debug

--- student/www2_student_turtle.cpp
+++ student/www2_student_turtle.cpp
@@ -146,6 +146,7 @@ turtleMove studentTurtleStep(bool bumped, int nw_or) {
     // Execute the appropriate move
     switch (currentState) {
         case STATE_MOVE_FORWARD:
+            ROS_INFO("ENTERS THIS CASE");
             updatePosition_turtle(targetDirection);
             incrementVisits(currentX, currentY);
             return MOVE_FORWARD;
