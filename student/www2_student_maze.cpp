2277e4b0f19b24277aa6927dc3ba8d102cb39f7a
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-10-24 23:52:32 -0400

    debug

--- student/www2_student_maze.cpp
+++ student/www2_student_maze.cpp
@@ -1,173 +1,8 @@
-// /* 
-//  *
-//  * STUDENT NAME: William Wang 
-//  * ANDREW ID: www2  
-//  * LAST UPDATE: 10/24/2024
-//  *
-//  * This file keeps track of where the turtle is in the maze
-//  * and updates the location when the turtle is moved. It shall not
-//  * contain the maze solving logic/algorithm.
-//  *
-//  * This file is used along with student_turtle.cpp. student_turtle.cpp shall
-//  * contain the maze solving logic/algorithm and shall not make use of the
-//  * absolute coordinates or orientation of the turtle.
-//  *
-//  * This file shall call studentTurtleStep(..) in student_turtle.cpp to determine
-//  * the next move the turtle will make, and shall use translatePos(..) and
-//  * translateOrnt(..) to translate this move into absolute coordinates
-//  * to display the turtle.
-//  *
-//  */
-
-// #include "student.h"
-
-// // Constants
-// const int32_t TIMEOUT = 40;           // Timer value to slow down the simulation for better visibility
-// const int32_t TIMER_EXPIRED = 0;      // Timer expired value
-// const int32_t TIME_DECREMENT = 1;     // Constant to decrement timer by
-// const int32_t MOVE_INCREMENT = 1;
-// const int32_t MOVE_DECREMENT = -1;
-
-// // Typedefs for future flexibility
-// typedef int32_t State;    // typedef for state representation
-// typedef bool Flag;        // typedef for boolean flag
-
-// // Defining struct for Position
-// typedef struct{
-//   int32_t X;
-//   int32_t Y;
-// } Position;
-
-// // Position Count
-// static int32_t relativeX = START_POS;
-// static int32_t relativeY = START_POS;
-
-// /**
-//  * @brief Updates the current turtle's position based on its direction.
-//  * The turtle moves forward by 1 unit based on the current direction.
-//  */
-// void updatePosition(int nw_or) {
-//     ROS_INFO("Update Position Called w/ Direction %d", nw_or);
-//     switch (nw_or) {
-//         case NORTH:
-//             relativeX -= 1;  // Move north (up in Y axis)
-//             break;
-//         case EAST:
-//             relativeY -= 1;  // Move east (right in X axis)
-//             break;
-//         case SOUTH:
-//             relativeX += 1;  // Move south (down in Y axis)
-//             break;
-//         case WEST:
-//             relativeY += 1;  // Move west (left in X axis)
-//             break;
-//     }
-// }
-
-// bool moveTurtle(QPointF& pos_, int& nw_or) {
-//     static int32_t timer = TIMEOUT;
-//     Flag bumpedFlag = false;
-//     Flag atEnd = false;
-
-//     if (timer == TIMER_EXPIRED) {
-//         bumpedFlag = bumped(pos_.x(), pos_.y(), pos_.x(), pos_.y());
-//         atEnd = atend(pos_.x(), pos_.y());
-
-//         if (atEnd) {
-//             return false;  // Stop the turtle if it reaches the goal
-//         }
-
-//         turtleMove nextMove = studentTurtleStep(bumpedFlag, nw_or);
-//         nw_or = translateOrnt(nw_or, nextMove);  // Update orientation
-
-//         if (nextMove == MOVE_FORWARD) {
-//             pos_ = translatePos(pos_, nextMove, nw_or);
-//             int visits = getVisits(relativeX, relativeY);  // Get visit count
-//             ROS_INFO("X = %d, Y = %d, visits = %d", relativeX, relativeY, visits);
-//             ROS_INFO("Moving Forward");
-//             displayVisits(visits);  // Update display with visit count
-//         }
-//     }
-
-//     timer = (timer == TIMER_EXPIRED) ? TIMEOUT : timer - TIME_DECREMENT;
-//     return (timer == TIMEOUT);
-// }
-
-// /*
-//  * Takes a position and a turtleMove and returns a new position
-//  * based on the move
-//  */
-// QPointF translatePos(QPointF pos_, turtleMove nextMove, int nw_or) {
-//   if (nextMove == MOVE_FORWARD){
-//     switch (nw_or){
-//       case EAST:
-//         pos_.setY(pos_.y() + MOVE_DECREMENT); // Move East (right)
-//         break;
-//       case SOUTH:
-//         pos_.setX(pos_.x() + MOVE_INCREMENT); // Move South (down)
-//         break;
-//       case WEST:
-//         pos_.setY(pos_.y() + MOVE_INCREMENT); // Move West (left)
-//         break;
-//       case NORTH:
-//         pos_.setX(pos_.x() + MOVE_DECREMENT); // Move North (up)
-//         break;
-//       default:
-//         ROS_ERROR("Invalid orientation value 2: %d", nw_or);
-//         break;
-//     }
-//     return pos_;
-//   } else {
-//     ROS_INFO("Next move is not MOVE_FORWARD, no position update");
-//   }
-// }
-
-// /*
-//  * Takes an orientation and a turtleMove and returns a new orienation
-//  * based on the move
-//  */
-// int translateOrnt(int orientation, turtleMove nextMove) {
-//   switch (orientation){
-//     case NORTH:
-//       if (nextMove == TURN_RIGHT) {
-//         orientation = EAST;  // Turn right to face East
-//       } else if (nextMove == TURN_LEFT) { 
-//         orientation = WEST;  // Turn left to face West if bumped
-//       } 
-//       break;
-//     case EAST:
-//       if (nextMove == TURN_RIGHT) {
-//         orientation = SOUTH;  // Turn right to face South
-//       } else if (nextMove == TURN_LEFT) { 
-//         orientation = NORTH;  // Turn left to face North if bumped
-//       }
-//       break;
-//     case SOUTH:
-//       if (nextMove == TURN_RIGHT) {
-//         orientation = WEST;  // Turn right to face West
-//       } else if (nextMove == TURN_LEFT) { 
-//         orientation = EAST;  // Turn left to face East if bumped
-//       } 
-//       break;
-//     case WEST:
-//       if (nextMove == TURN_RIGHT) {
-//         orientation = NORTH;  // Turn right to face North
-//       } else if (nextMove == TURN_LEFT) { 
-//         orientation = SOUTH;  // Turn left to face South if bumped
-//       } 
-//       break;
-//     default:
-//       ROS_ERROR("Invalid orientation value 3: %d", orientation);
-//   }
-//   return orientation;  // Make sure to return the updated orientation
-// }
 /* 
- * Originally by Philip Koopman (koopman@cmu.edu)
- * and Milda Zizyte (milda@cmu.edu)
  *
  * STUDENT NAME: William Wang 
  * ANDREW ID: www2  
- * LAST UPDATE: 9/8/2024
+ * LAST UPDATE: 10/24/2024
  *
  * This file keeps track of where the turtle is in the maze
  * and updates the location when the turtle is moved. It shall not
@@ -212,6 +47,7 @@ static int32_t relativeY = START_POS;
  * The turtle moves forward by 1 unit based on the current direction.
  */
 void updatePosition(int nw_or) {
+    ROS_INFO("Update Position Called w/ Direction %d", nw_or);
     switch (nw_or) {
         case NORTH:
             relativeX -= 1;  // Move north (up in Y axis)
@@ -228,78 +64,33 @@ void updatePosition(int nw_or) {
     }
 }
 
-/*
- * This procedure takes the current turtle position and orientation and returns true=accept changes, false=do not accept changes
- * Ground rule -- you are only allowed to call the three helper functions defined in student.h, and NO other turtle methods or maze methods (no peeking at the maze!)
- * This file interfaces with functions in student_turtle.cpp
- * 
- * nw_or = orientation pos_ = position
- */
-bool moveTurtle(QPointF& pos_, int& nw_or) 
-{
-  // return studentMoveTurtle(pos_, nw_or);
-  static int32_t timer = TIMEOUT;                   // timer for managing movement
-  
-  Position futureX1, futureY1, futureX2, futureY2;  // future posistions based on orientation
-  Flag bumpedFlag = false;
-  Flag atEnd = false;
+bool moveTurtle(QPointF& pos_, int& nw_or) {
+    static int32_t timer = TIMEOUT;
+    Flag bumpedFlag = false;
+    Flag atEnd = false;
 
-  if (timer == TIMER_EXPIRED){
-    int old_nw_or = nw_or;
-    Flag shouldMove = false; // Flag to determine if turtle should move
-    futureX1.X = pos_.x();
-    futureY1.Y = pos_.y();
-    futureX2.X = pos_.x();
-    futureY2.Y = pos_.y();
-    switch (nw_or){
-      case NORTH: // moving north increases Y
-        futureY2.Y += MOVE_INCREMENT; 
-        break;
-      case EAST: // moving east increases x
-        futureX2.X += MOVE_INCREMENT; 
-        break;
-      case SOUTH: // moving south increases both X and Y (diagonal)
-        futureX2.X += MOVE_INCREMENT;
-        futureY2.Y += MOVE_INCREMENT; 
-        futureX1.X += MOVE_INCREMENT;
-        break;
-      case WEST: // moving west increases both X and Y (diagonal)
-        futureX2.X += MOVE_INCREMENT;
-        futureY2.Y += MOVE_INCREMENT; 
-        futureY1.Y += MOVE_INCREMENT;
-        break;
-      default:
-        ROS_ERROR("Invalid orientation value 1: %d", nw_or);
-        break;
+    if (timer == TIMER_EXPIRED) {
+        bumpedFlag = bumped(pos_.x(), pos_.y(), pos_.x(), pos_.y());
+        atEnd = atend(pos_.x(), pos_.y());
+
+        if (atEnd) {
+            return false;  // Stop the turtle if it reaches the goal
+        }
+
+        turtleMove nextMove = studentTurtleStep(bumpedFlag, nw_or);
+        nw_or = translateOrnt(nw_or, nextMove);  // Update orientation
+
+        if (nextMove == MOVE_FORWARD) {
+            pos_ = translatePos(pos_, nextMove, nw_or);
+            int visits = getVisits(relativeX, relativeY);  // Get visit count
+            ROS_INFO("X = %d, Y = %d, visits = %d", relativeX, relativeY, visits);
+            ROS_INFO("Moving Forward");
+            displayVisits(visits);  // Update display with visit count
+        }
     }
 
-    bumpedFlag = bumped(futureX1.X, futureY1.Y, futureX2.X, futureY2.Y);
-    atEnd = atend(pos_.x(), pos_.y());
-
-    // Call to studentTurtleStep() to determine next step based on whether a bump occurred
-    turtleMove nextMove = studentTurtleStep(bumpedFlag, nw_or);
-
-
-    nw_or = translateOrnt(nw_or, nextMove); // update orientation
-    shouldMove = (nextMove == MOVE_FORWARD);
-
-    if (shouldMove && !atEnd) {
-      QPointF old_pos_ = pos_;
-      pos_ = translatePos(pos_, nextMove, old_nw_or);            // updates Position
-
-      updatePosition(nw_or);
-      int visits = getVisits(relativeX, relativeY);  // Get the visit count for the current position
-      displayVisits(visits);  // Update the display with the visit count
-      shouldMove = false;
-    }
-  }
-
-  if (atEnd){
-    return false;
-  }
-
-  timer = (timer == TIMER_EXPIRED) ? TIMEOUT : timer - TIME_DECREMENT;
-  return (timer == TIMEOUT);
+    timer = (timer == TIMER_EXPIRED) ? TIMEOUT : timer - TIME_DECREMENT;
+    return (timer == TIMEOUT);
 }
 
 /*
@@ -368,6 +159,5 @@ int translateOrnt(int orientation, turtleMove nextMove) {
     default:
       ROS_ERROR("Invalid orientation value 3: %d", orientation);
   }
-
   return orientation;  // Make sure to return the updated orientation
 }
--- student/www2_student_turtle.cpp
+++ student/www2_student_turtle.cpp
@@ -1,166 +1,3 @@
-// /*
-//  * Originally by Philip Koopman (koopman@cmu.edu)
-//  * and Milda Zizyte (milda@cmu.edu)
-//  *
-//  * STUDENT NAME: William Wang
-//  * ANDREW ID: www2    
-//  * LAST UPDATE: 9/8/2024
-//  *
-//  * This file is an algorithm to solve the ece642rtle maze
-//  * using the right-hand rule.
-//  */
-
-// #include "student.h"
-// #include <stdint.h>  // Include stdint.h for fixed-width integer types
-
-// // Constants for various states and timeout values
-// const int32_t STATE_MOVE_FORWARD = 0;
-// const int32_t STATE_TURN_LEFT = 1;
-// const int32_t STATE_TURN_RIGHT = 2;
-// const int32_t MAZE_SIZE = 100;         // size of internal tracking array (23x23)
-// static int32_t relativeX = START_POS;
-// static int32_t relativeY = START_POS;
-// static int32_t currentX = START_POS;  // Current relative X position of the turtle
-// static int32_t currentY = START_POS;  // Current relative Y position of the turtle
-
-
-// // Typedefs of state and flag
-// typedef int32_t State;       // Typedef for state representation
-// typedef bool Flag;           // Typedef for boolean flags
-
-// // Static array to keep track of visits to each cell
-// static int32_t visitMap[MAZE_SIZE][MAZE_SIZE] = {0}; // All cells initialized to zero
-
-// /**
-//  *  @brief Retrieve visits at specific (x,y) coordinate
-//  * 
-//  * */ 
-// int32_t getVisits(int32_t x, int32_t y) {
-//     // Check if the x and y coordinates are within the valid range of the visitMap array
-//     if (x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE) {
-//         return visitMap[x][y];  // Return the visit count if coordinates are valid
-//     } else {
-//         ROS_ERROR("Invalid coordinates (%d, %d) accessed in visitMap.", x, y);
-//         return 0;  // Return 0 or an appropriate default value for out-of-bounds coordinates
-//     }
-// }
-
-// /**
-//  * @brief Function to increment the number of visits to a specific cell.
-//  */
-// void incrementVisits(int32_t x, int32_t y) {
-//     // Check if the x and y coordinates are within the valid range of the visitMap array
-//     if (x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE) {
-//         ROS_INFO("GOES HERE AND INCREMENTS");
-//         visitMap[x][y]++;  // Increment the visit count if coordinates are valid
-//     } else {
-//         ROS_ERROR("Invalid coordinates (%d, %d) accessed in visitMap during increment.", x, y);
-//     }
-// }
-
-// /**
-//  * @brief Updates the current turtle's position based on its direction.
-//  * The turtle moves forward by 1 unit based on the current direction.
-//  */
-// void updatePosition_turtle(int nw_or) {
-//     switch (nw_or) {
-//         case NORTH:
-//             currentX -= 1;  // Move north (up in Y axis)
-//             break;
-//         case EAST:
-//             currentY -= 1;  // Move east (right in X axis)
-//             break;
-//         case SOUTH:
-//             currentX += 1;  // Move south (down in Y axis)
-//             break;
-//         case WEST:
-//             currentY += 1;  // Move west (left in X axis)
-//             break;
-//     }
-// }
-
-// /**
-//  * @brief Determine if the turtle can move in a certain direction and return the number of visits to that square
-//  */
-// int checkDirection(int direction) {
-//     switch (direction) {
-//         case NORTH:
-//             return (currentX > 0) ? getVisits(currentX - 1, currentY) : -1;
-//         case EAST:
-//             return (currentY < MAZE_SIZE - 1) ? getVisits(currentX, currentY + 1) : -1;
-//         case SOUTH:
-//             return (currentX < MAZE_SIZE - 1) ? getVisits(currentX + 1, currentY) : -1;
-//         case WEST:
-//             return (currentY > 0) ? getVisits(currentX, currentY - 1) : -1;
-//         default:
-//             return -1;
-//     }
-// }
-
-// /**
-//  * @brief Calculating turns based on direction
-//  */
-// int calculateTurns(int currentDirection, int targetDirection) {
-//     // Calculate the difference in direction mod 4
-//     int turns = (targetDirection - currentDirection + 4) % 4;
-    
-//     // If the number of turns is more than 2, it's faster to turn the other way
-//     return (turns > 2) ? (4 - turns) : turns;
-// }
-
-// /**
-//  * @brief turtle's next action based orientation nw_or and bumped status
-//  */
-// turtleMove studentTurtleStep(bool bumped, int nw_or) {
-//     static State currentState = STATE_MOVE_FORWARD;
-//     static int numTurns = 0;
-
-//     ROS_INFO("currentState = %d", currentState);
-
-//     // Default to the current direction
-//     int targetDirection = nw_or;
-//     int minVisits = INT32_MAX;
-
-//     ROS_INFO("Target Direction: %d", targetDirection);
-
-//     // Check all four directions and find the optimal target direction
-//     for (int i = 0; i < 4; ++i) {
-//         int visits = checkDirection(i);
-//         if (visits != -1 && visits < minVisits) {
-//             minVisits = visits;
-//             targetDirection = i;
-//         }
-//     }
-
-//     // Calculate the number of turns required to face the target direction
-//     numTurns = calculateTurns(nw_or, targetDirection);
-
-//     // State transition logic based on the number of turns and whether the turtle bumped
-//     if (numTurns > 0) {
-//         currentState = STATE_TURN_LEFT;  // Turn towards the target direction
-//     } else if (bumped) {
-//         currentState = STATE_TURN_LEFT;  // If blocked, keep turning left
-//     } else {
-//         currentState = STATE_MOVE_FORWARD;
-//     }
-
-//     // Execute the appropriate move
-//     switch (currentState) {
-//         case STATE_MOVE_FORWARD:
-//             ROS_INFO("ENTERS THIS CASE");
-//             updatePosition_turtle(targetDirection);
-//             incrementVisits(currentX, currentY);
-//             return MOVE_FORWARD;
-//         case STATE_TURN_LEFT:
-//             numTurns--;
-//             return TURN_LEFT;
-//         case STATE_TURN_RIGHT:
-//             return TURN_RIGHT;
-//         default:
-//             return MOVE_FORWARD;
-//     }
-// }
-
 /*
  * Originally by Philip Koopman (koopman@cmu.edu)
  * and Milda Zizyte (milda@cmu.edu)
@@ -176,12 +13,10 @@
 #include "student.h"
 #include <stdint.h>  // Include stdint.h for fixed-width integer types
 
-// Define size of the maze array
-
 // Constants for various states and timeout values
-const int32_t STATE_MOVE_FORWARD = 2;
-const int32_t STATE_TURN_LEFT = 0;
-const int32_t STATE_TURN_RIGHT = 1;
+const int32_t STATE_MOVE_FORWARD = 0;
+const int32_t STATE_TURN_LEFT = 1;
+const int32_t STATE_TURN_RIGHT = 2;
 const int32_t MAZE_SIZE = 100;         // size of internal tracking array (23x23)
 static int32_t relativeX = START_POS;
 static int32_t relativeY = START_POS;
@@ -189,13 +24,17 @@ static int32_t currentX = START_POS;  // Current relative X position of the turt
 static int32_t currentY = START_POS;  // Current relative Y position of the turtle
 
 
-// Typedefs for readability and future flexibility
+// Typedefs of state and flag
 typedef int32_t State;       // Typedef for state representation
 typedef bool Flag;           // Typedef for boolean flags
 
 // Static array to keep track of visits to each cell
 static int32_t visitMap[MAZE_SIZE][MAZE_SIZE] = {0}; // All cells initialized to zero
 
+/**
+ *  @brief Retrieve visits at specific (x,y) coordinate
+ * 
+ * */ 
 int32_t getVisits(int32_t x, int32_t y) {
     // Check if the x and y coordinates are within the valid range of the visitMap array
     if (x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE) {
@@ -206,19 +45,18 @@ int32_t getVisits(int32_t x, int32_t y) {
     }
 }
 
-
 /**
  * @brief Function to increment the number of visits to a specific cell.
  */
 void incrementVisits(int32_t x, int32_t y) {
     // Check if the x and y coordinates are within the valid range of the visitMap array
     if (x >= 0 && x < MAZE_SIZE && y >= 0 && y < MAZE_SIZE) {
+        ROS_INFO("GOES HERE AND INCREMENTS");
         visitMap[x][y]++;  // Increment the visit count if coordinates are valid
     } else {
         ROS_ERROR("Invalid coordinates (%d, %d) accessed in visitMap during increment.", x, y);
     }
 }
-
 
 /**
  * @brief Updates the current turtle's position based on its direction.
@@ -242,28 +80,79 @@ void updatePosition_turtle(int nw_or) {
 }
 
 /**
- * @brief Function decided the next move the turtle should make.
- * `bumpedFlag` tells us whether the turtle hit a wall in front.
+ * @brief Determine if the turtle can move in a certain direction and return the number of visits to that square
+ */
+int checkDirection(int direction) {
+    switch (direction) {
+        case NORTH:
+            return (currentX > 0) ? getVisits(currentX - 1, currentY) : -1;
+        case EAST:
+            return (currentY < MAZE_SIZE - 1) ? getVisits(currentX, currentY + 1) : -1;
+        case SOUTH:
+            return (currentX < MAZE_SIZE - 1) ? getVisits(currentX + 1, currentY) : -1;
+        case WEST:
+            return (currentY > 0) ? getVisits(currentX, currentY - 1) : -1;
+        default:
+            return -1;
+    }
+}
+
+/**
+ * @brief Calculating turns based on direction
+ */
+int calculateTurns(int currentDirection, int targetDirection) {
+    // Calculate the difference in direction mod 4
+    int turns = (targetDirection - currentDirection + 4) % 4;
+    
+    // If the number of turns is more than 2, it's faster to turn the other way
+    return (turns > 2) ? (4 - turns) : turns;
+}
+
+/**
+ * @brief turtle's next action based orientation nw_or and bumped status
  */
 turtleMove studentTurtleStep(bool bumped, int nw_or) {
-    static State currentState = STATE_MOVE_FORWARD; // Current state of the turtle's movement
-    
-    // returns the move back to maze on what to do (depends on current state & whether it has bumped)
-    if (currentState == STATE_MOVE_FORWARD){
-        currentState = STATE_TURN_RIGHT;
-    } else if (bumped){
-        currentState = STATE_TURN_LEFT;
+    static State currentState = STATE_MOVE_FORWARD;
+    static int numTurns = 0;
+
+    ROS_INFO("currentState = %d", currentState);
+
+    // Default to the current direction
+    int targetDirection = nw_or;
+    int minVisits = INT32_MAX;
+
+    ROS_INFO("Target Direction: %d", targetDirection);
+
+    // Check all four directions and find the optimal target direction
+    for (int i = 0; i < 4; ++i) {
+        int visits = checkDirection(i);
+        if (visits != -1 && visits < minVisits) {
+            minVisits = visits;
+            targetDirection = i;
+        }
+    }
+
+    // Calculate the number of turns required to face the target direction
+    numTurns = calculateTurns(nw_or, targetDirection);
+
+    // State transition logic based on the number of turns and whether the turtle bumped
+    if (numTurns > 0) {
+        currentState = STATE_TURN_LEFT;  // Turn towards the target direction
+    } else if (bumped) {
+        currentState = STATE_TURN_LEFT;  // If blocked, keep turning left
     } else {
         currentState = STATE_MOVE_FORWARD;
     }
 
-    // return turtleMove based on defined current state
-    switch (currentState){
+    // Execute the appropriate move
+    switch (currentState) {
         case STATE_MOVE_FORWARD:
-            updatePosition_turtle(nw_or); 
-            incrementVisits(currentX, currentY); 
+            ROS_INFO("ENTERS THIS CASE");
+            updatePosition_turtle(targetDirection);
+            incrementVisits(currentX, currentY);
             return MOVE_FORWARD;
         case STATE_TURN_LEFT:
+            numTurns--;
             return TURN_LEFT;
         case STATE_TURN_RIGHT:
             return TURN_RIGHT;
