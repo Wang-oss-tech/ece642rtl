f3cb3cd2ac857ed42d1ccf151d6e4d8291530878
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-10-26 01:27:28 -0400

    revert

--- student/www2_student_maze.cpp
+++ student/www2_student_maze.cpp
@@ -110,6 +110,7 @@ bool moveTurtle(QPointF& pos_, int& nw_or)
         ROS_ERROR("Invalid orientation value 1: %d", nw_or);
         break;
     }
+    ROS_INFO("Future Turtle Pos (X,Y): %d, %d", futureX2.X, futureY2.Y);
     bumpedFlag = bumped(futureX1.X, futureY1.Y, futureX2.X, futureY2.Y);
     ROS_INFO("Computed bumped %d", bumpedFlag);
     atEnd = atend(pos_.x(), pos_.y());
