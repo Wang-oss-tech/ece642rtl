4a78b3484958068ec80fd22ae6911753a9c7fea9
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-11-14 09:38:08 -0500

    new data coverage tests

--- student/www2_student_test.cpp
+++ student/www2_student_test.cpp
@@ -43,7 +43,7 @@ void test_T3_numTurnsEquals3() {
         std::pair<turtleMove, int> result = studentTurtleStep(false, NORTH);
     }
 
-    std::pair<turtleMove, int> result = studentTurtleStep(false, SOUTH);
+    std::pair<turtleMove, int> result = studentTurtleStep(false, WEST);
     CU_ASSERT_EQUAL(result.first, MOVE_FORWARD);
     CU_ASSERT_EQUAL(result.second, 0);
 }
