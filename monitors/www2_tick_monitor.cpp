afe2da51659eaa0794259f5bdad7d3aad17fb244
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-11-19 16:51:54 -0500

    new monitor

--- monitors/www2_tick_monitor.cpp
+++ monitors/www2_tick_monitor.cpp
@@ -29,7 +29,12 @@ static std::map<int, int> opp_orientations {
 };
 
 // tick called fist time
-static bool tick_first = true;
+static bool tick_first = false;
+
+// counter variables for each interrupts
+static int pose_interrupt_cnt = 0;
+static int visit_interrupt_cnt = 0;
+static int bump_interrupt_cnt = 0;
 
 // current orienation of tutle 
 static Orientation curr_or;
@@ -38,21 +43,29 @@ static Orientation curr_or;
 static Orientation prev_or;
 
 void tickInterrupt(ros::Time t) {
-    if (tick_first == true){
-        prev_or = curr_or;
-        tick_first = false;
-    } else {
-        ROS_INFO("[[%ld ns]] 'Tick' was sent. Data: orientation = %s", 
-                    t.toNSec(), 
-                    or_string[curr_or].c_str());
+    if (tick == true) {
+		if (pose <= 1 && visit <= 1 && bump <= 1) {
+            ROS_INFO("[[%ld ns]] 'Tick' received", 
+                    t.toNSec());
+		} else {
+			ROS_WARN("VIOLATION: 'Tick' recieved but
+                    more than one poseInterrupt, visitsInterrupt, 
+                    and bumpInterrupt called\n
+                    pose_interrupt_cnt: %d \n
+                    visit_interrupt_cnt: %d \n
+                    bump_interrupt_cnt: %d \n",
+                    pose_interrupt_cnt, visit_interrupt_cnt, 
+                    bump_interrupt_cnt);
+		}
+	} else {
+		tick = true;
+	}
 
-        if (opp_orientations[curr_or] == prev_or) {
-            ROS_WARN("VIOLATION: Turned  %s to %s", 
-                     or_string[curr_or].c_str(), 
-                     or_string[prev_or].c_str());
-        }
-        prev_or = curr_or;
-    }
+    // reset counters
+    pose_interrupt_cnt = 0;
+    visit_interrupt_cnt = 0;
+    bump_interrupt_cn = 0;
+
 }
 
 /*
@@ -61,7 +74,7 @@ void tickInterrupt(ros::Time t) {
  * if the locations differ by more than 1 in Manhattan Distance.
  */
 void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
-    curr_or = o; 
+    pose_interrupt_cnt = pose_interrupt_cnt + 1;
 }
 
 /*
@@ -69,10 +82,12 @@ void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
  */
 
 void visitInterrupt(ros::Time t, int visits) {
+    visit_interrupt_cnt = visit_interrupt_cnt + 1;
 }
 
 
 void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
+    bump_interrupt_cnt = bump_interrupt_cnt + 1;
 }
 
 void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
