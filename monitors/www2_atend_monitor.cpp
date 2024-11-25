18804333615cb624462385a60e3acdcfac9f15e6
William Wang <68651654+Wang-oss-tech@users.noreply.github.com>
2024-11-24 19:57:28 -0500

    new tar

--- monitors/www2_atend_monitor.cpp
+++ monitors/www2_atend_monitor.cpp
@@ -11,43 +11,50 @@
 #include <string>
 #include "monitor_interface.h"
 
-// inital status call for poseInterrupt
+// Initial status call for poseInterrupt
 static bool init_pos = false;
 
-// current position
+// Current position
 static Pose current_position;
 
-// interrupt called when position and orientation is updated
+// Indicates if the turtle has reached the goal (to output success messages)
+static bool reached_goal = false;
+
+// Interrupt called when position and orientation are updated
 void poseInterrupt(ros::Time t, int x, int y, Orientation o) {
     current_position.x = x;
     current_position.y = y; 
 }
 
-// interrupt occurs when atend(x,y) returns
+// Interrupt occurs when atEnd(x, y) is called
 void atEndInterrupt(ros::Time t, int x, int y, bool atEnd) {
-    if (init_pos == true){
-        if(current_position.x == x &&
-           current_position.y == y){
-            ROS_INFO("[[%ld ns]] AtEnd at correct final location [%d,%d]", 
-                      t.toNSec(), x, y);
-        } else{
-            ROS_WARN("VIOLATION: Current position is [%d, %d]"
-                      "with supposed AtEnd location at [%d, %d]",
-                      current_position.x, current_position.y,
-                      x, y);
+    if (init_pos) {
+        if (current_position.x == x && current_position.y == y) {
+            if (atEnd) {
+                // Turtle is at the correct position and correctly calls atEnd
+                reached_goal = true;
+            } 
+            ROS_INFO("Successful atEnd at [%d, %d] at time [%ld ns]", x, y, t.toNSec());
+        } else {
+            // Violation: Turtle is not at the position it is calling atEnd for
+            ROS_WARN("VIOLATION: Current position is [%d, %d] with supposed atEnd location at [%d, %d]",
+                     current_position.x, current_position.y, x, y);
         }
     }
     init_pos = true;
 }
 
-/* Empty Interrupts*/
+// Tick interrupt occurs periodically (outputs success messages to flush the queue)
 void tickInterrupt(ros::Time t) {
+    if (reached_goal) {
+        ROS_INFO("Flushing output queue: Turtle successfully reached the end.");
+    }
 }
 
-
+// Interrupt for visit count (not used in this monitor)
 void visitInterrupt(ros::Time t, int visits) {
 }
 
-// interrupt occurs when bumped() is called 
+// Interrupt occurs when bumped() is called (not used in this monitor)
 void bumpInterrupt(ros::Time t, int x1, int y1, int x2, int y2, bool bumped) {
 }
